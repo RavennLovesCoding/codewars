3599ea31c8a9a27f24db705e1cddb153346aad59
Ravenn <RavennMB@Gmail.com>
2024-09-18 08:07:04 -0600

    completed one challenge, uploading code

---
+++ solutions.js
@@ -0,0 +1,15 @@
+/* I will upload my coadwars challenge solutions, as I do them in that order. */
+
+/* 8 kyu Grasshopper - Debug sayHello
+
+The starship Enterprise has run into some problem when creating a program to greet everyone as they come aboard. It is your job to fix the code and get the program working again!
+
+Example output:
+
+Hello, Mr. Spock */
+
+//solution
+function sayHello(name) {
+    return `Hello, ${name}`
+  }
+
