6c08f0fbf9b0213d006d71be83cf7e47e4eb88e2
Ravenn <RavennMB@Gmail.com>
2024-10-19 21:22:10 -0600

    8 kyu Fix your code before the garden dies!

--- 8kyuusolutions.js
+++ 8kyuusolutions.js
@@ -109,3 +109,18 @@ P.S. Each array includes only integer numbers. Output is a number too.*/
       return total;
       
     }
+
+    //8 kyu Fix your code before the garden dies!
+
+    /* You have an award-winning garden and every day the plants need exactly 40mm of water. You created a great piece of JavaScript to calculate the amount of water your plants will need when you have taken into consideration the amount of rain water that is forecast for the day. Your jealous neighbour hacked your computer and filled your code with bugs.
+
+Your task is to debug the code before your plants die! */
+
+function rainAmount(mm){
+  let waterAmount = null;
+  if( mm < 40) {waterAmount = (40 - mm)};
+  if (mm < 40) {
+    return `You need to give your plant ${waterAmount}mm of water`;
+  } else { return "Your plant has had more than enough water for today!"; }
+  
+}
