538a2ce1814488939c99e8f3b82b534b1fd7043d
Ravenn <RavennMB@Gmail.com>
2024-10-19 20:44:20 -0600

    completed 101 dalmations 8kyuu

--- 8kyuusolutions.js
+++ 8kyuusolutions.js
@@ -54,3 +54,29 @@ function bonusTime(salary, bonus) {
       return "\u00A3" +salary;
     }
   }
+
+//8 kyu 101 Dalmatians - squash the bugs, not the dogs!
+
+/* Your friend has been out shopping for puppies (what a time to be alive!)... He arrives back with multiple dogs, and you simply do not know how to respond!
+
+By repairing the function provided, you will find out exactly how you should respond, depending on the number of dogs he has.
+
+The number of dogs will always be a number and there will always be at least 1 dog. */
+
+
+  function howManyDalmatians(number) {
+  
+    const dogs = ["Hardly any", "More than a handful!", "Woah that's a lot of dogs!", "101 DALMATIANS!!!"];
+    
+   
+       if (number <= 10) { 
+         return dogs[0]
+       } else if (number <= 50 && number >= 11) {
+          return dogs[1]
+        } else if (number >= 101) {
+          return dogs[3]
+        } else {
+          return dogs[2]
+        }
+  
+    }
