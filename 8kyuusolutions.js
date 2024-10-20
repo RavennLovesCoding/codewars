7992c7035ca68063c837e6b11bbf0f9fbb4cf5bb
Ravenn <RavennMB@Gmail.com>
2024-10-19 22:30:01 -0600

    kyuu jennys secret message

--- 8kyuusolutions.js
+++ 8kyuusolutions.js
@@ -124,3 +124,15 @@ function rainAmount(mm){
   } else { return "Your plant has had more than enough water for today!"; }
   
 }
+
+//8 kyu Jenny's secret message
+
+/* Jenny has written a function that returns a greeting for a user. However, she's in love with Johnny, and would like to greet him slightly different. She added a special case to her function, but she made a mistake.
+
+Can you help her?*/
+
+function greet(name){
+  if(name === "Johnny")
+    return "Hello, my love!";
+  else {return `Hello, ${name}!`;}
+}
