2a4bd76226211d5b10ce12f5b26c75ccacec17c6
Ravenn <ravennmb@gmail.com>
2025-04-25 20:09:24 -0600

    completed 8kyuu count the monkeys problem

--- 8kyuusolutions.js
+++ 8kyuusolutions.js
@@ -243,3 +243,22 @@ function removeChar(str){
  return nuStr;
  };
  
+
+ //Count the Monkeys!
+
+ /* 
+ You take your son to the forest to see the monkeys. You know that there are a certain number there (n), but your son is too young to just appreciate the full number, he has to start counting them from 1.
+
+As a good parent, you will sit and count with him. Given the number (n), populate an array with all numbers up to and including that number, but excluding zero.
+ */
+
+//solution
+
+function monkeyCount(n) {
+  // your code here
+    let result = [];
+    for (let i = 1; i <= n; i++) {
+      result.push(i);
+    }
+    return result;
+  }
